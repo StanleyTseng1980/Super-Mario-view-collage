8d942f1ba23417528f93a7133400468f29344e4b
stanleytseng <stanley690316@gmail.com>
2019-11-07 12:00:04 +0800

    Initial Commit

---
+++ viewMario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewMario
+//
+//  Created by Stanley Tseng on 2019/11/6.
+//  Copyright © 2019 StanleyAppWorld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
